4061237e3c947f45815f0693d3389f1ccd591200
Do Gia Dat <dogiadat@gmail.com>
2018-11-30 18:09:00 +0700

    feature: fetch commits

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-test = () => console.log("ok");
+import { spawn } from 'child_process'
+import { parseCommits, LOG_FORMAT } from './commits'
 
-test();
+
+function cmd (cmd, args = [], options = {}) {
+  return new Promise((resolve, reject) => {
+    const child = spawn(cmd, args, options)
+    let data = ''
+
+    child.stdout.on('data', buffer => data += buffer.toString())
+    child.stdout.on('end', () => resolve(data))
+    child.on('error', reject)
+  })
+}
+
+function getCommits () {
+  return cmd('git', ['log', '--shortstat', '--pretty=format:' + LOG_FORMAT]).then(parseCommits)
+}
+
+Promise.all([ getCommits()]).then(([ commits]) => {
+  console.log(commits)
+})
