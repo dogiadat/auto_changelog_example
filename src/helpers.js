fe039d7c5b3c90e129de2e8bbc6ac853e153aeae
Do Gia Dat <dogiadat@gmail.com>
2018-12-03 13:39:07 +0700

    feature: fetch origin

---
+++ src/helpers.js
@@ -0,0 +1,12 @@
+import { spawn } from 'child_process'
+
+export function cmd(cmd, args = [], options = {}) {
+  return new Promise((resolve, reject) => {
+    const child = spawn(cmd, args, options)
+    let data = ''
+
+    child.stdout.on('data', buffer => data += buffer.toString())
+    child.stdout.on('end', () => resolve(data))
+    child.on('error', reject)
+  })
+}
